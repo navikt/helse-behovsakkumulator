753b1c10f413d5b8dc72d0bdc748a170af2224b8
Jakob Havstein Eriksen <jakob.havstein.eriksen@nav.no>
2019-12-02 14:20:30 +0100

    Gjør akkumulator-logikken mer lesbar
    Co-authored-by: Truls Jørgensen <truls.jorgensen@nav.no>

--- src/main/kotlin/no/nav/helse/behovsakkumulator/App.kt
+++ src/main/kotlin/no/nav/helse/behovsakkumulator/App.kt
@@ -8,7 +8,6 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.fasterxml.jackson.module.kotlin.treeToValue
 import io.ktor.application.install
-import io.ktor.client.features.json.JacksonSerializer
 import io.ktor.metrics.micrometer.MicrometerMetrics
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
@@ -17,19 +16,15 @@ import io.micrometer.prometheus.PrometheusConfig
 import io.micrometer.prometheus.PrometheusMeterRegistry
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineExceptionHandler
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.runBlocking
 import net.logstash.logback.argument.StructuredArguments.keyValue
-import org.apache.kafka.clients.producer.KafkaProducer
-import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.serialization.Serdes
-import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.streams.KafkaStreams
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.Consumed
+import org.apache.kafka.streams.kstream.KStream
 import org.apache.kafka.streams.kstream.Produced
 import org.slf4j.LoggerFactory
 import java.util.Properties
@@ -84,46 +79,62 @@ fun createStream(
     environment: Environment,
     serviceUser: ServiceUser,
     baseConfig: Properties = loadBaseConfig(environment, serviceUser)
-) : KafkaStreams {
+): KafkaStreams {
     val builder = StreamsBuilder()
     builder
-        .stream<String, JsonNode>(environment.spleisBehovtopic, Consumed.with(Serdes.StringSerde(), JacksonKafkaSerde()))
-        .filter { _, value -> value.hasNonNull("@løsning") }
-        .filterNot { _, value -> value["final"]?.asBoolean() == true }
-        .peek { key, value ->
-            log.info("Mottok {} for behov med {}",
-                keyValue("løsninger", value["@løsning"].fieldNames().asSequence().joinToString(", ")),
-                keyValue("id", key)
-            )
-        }
-        .groupByKey()
-        .reduce { behov1, behov2 ->
-            val result = behov1.deepCopy<ObjectNode>()
-            val løsning = result["@løsning"] as ObjectNode
-            behov2["@løsning"].fields().forEach { (key, value) ->
-                løsning.set(key, value)
-            }
-            result
-        }
+        .stream<String, JsonNode>(
+            environment.spleisBehovtopic,
+            Consumed.with(Serdes.StringSerde(), JacksonKafkaSerde())
+        )
+        .alleBehovSomIkkeErMarkertFerdig()
+        .kombinerDelløsningerPåBehov()
+        .bareKomplettLøsningPåBehov()
+        .markerBehovFerdig()
+        .to(environment.spleisBehovtopic, Produced.with(Serdes.StringSerde(), JacksonKafkaSerde()))
+    return KafkaStreams(builder.build(), baseConfig.toStreamsConfig())
+}
+
+private fun KStream<String, JsonNode>.markerBehovFerdig(): KStream<String, JsonNode> =
+    this.mapValues { value -> (value as ObjectNode).put("final", true) as JsonNode }
+        .peek { key, _ -> log.info("Markert behov med {} som final", keyValue("id", key)) }
+
+private fun KStream<String, JsonNode>.kombinerDelløsningerPåBehov(): KStream<String, JsonNode> =
+    this.groupByKey()
+        .reduce(::slåSammenLøsninger)
         .toStream()
         .peek { key, value ->
-            log.info("Satt sammen {} for behov med id {}. Forventer {}",
+            log.info(
+                "Satt sammen {} for behov med id {}. Forventer {}",
                 keyValue("løsninger", value["@løsning"].fieldNames().asSequence().joinToString(", ")),
                 keyValue("id", key),
                 keyValue("behov", value["behov"].asSequence().map(JsonNode::asText).joinToString(", "))
             )
         }
-        .filter { _, value ->
-            val løsninger = objectMapper.treeToValue<Map<String, JsonNode>>(value["@løsning"])
-            val behov = objectMapper.treeToValue<List<String>>(value["behov"])
 
-            behov.all(løsninger::containsKey)
-        }
-        .mapValues { value: JsonNode ->
-            (value as ObjectNode).put("final", true) as JsonNode
-        }
-        .peek{ key, _ -> log.info("Markert behov med {} som final", keyValue("id", key)) }
-        .to(environment.spleisBehovtopic, Produced.with(Serdes.StringSerde(), JacksonKafkaSerde()))
 
-    return KafkaStreams(builder.build(), baseConfig.toStreamsConfig())
+private fun KStream<String, JsonNode>.bareKomplettLøsningPåBehov(): KStream<String, JsonNode> =
+    this.filter { _, value ->
+        val løsninger = objectMapper.treeToValue<Map<String, JsonNode>>(value["@løsning"])
+        val behov = objectMapper.treeToValue<List<String>>(value["behov"])
+        behov.all(løsninger::containsKey)
+    }
+
+private fun KStream<String, JsonNode>.alleBehovSomIkkeErMarkertFerdig(): KStream<String, JsonNode> =
+    this.filter { _, value -> value.hasNonNull("@løsning") }
+        .filterNot { _, value -> value["final"]?.asBoolean() == true }
+        .peek { key, value ->
+            log.info(
+                "Mottok {} for behov med {}",
+                keyValue("løsninger", value["@løsning"].fieldNames().asSequence().joinToString(", ")),
+                keyValue("id", key)
+            )
+        }
+
+private fun slåSammenLøsninger(behov1: JsonNode, behov2: JsonNode): ObjectNode {
+    val result = behov1.deepCopy<ObjectNode>()
+    val løsning = result["@løsning"] as ObjectNode
+    behov2["@løsning"].fields().forEach { (key, value) ->
+        løsning.set(key, value)
+    }
+    return result
 }
