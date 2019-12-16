500a9cd9e7ec3de450d55f9a58dad47dd727cc60
David Steinsland <125661+davidsteinsland@users.noreply.github.com>
2019-12-16 22:16:10 +0100

    Mount inn standard vault-path også

--- src/main/kotlin/no/nav/helse/behovsakkumulator/Config.kt
+++ src/main/kotlin/no/nav/helse/behovsakkumulator/Config.kt
@@ -4,7 +4,7 @@ import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
 
-const val vaultBase = "/var/run/secrets/nais.io/vault"
+const val vaultBase = "/var/run/secrets/nais.io/serviceuser"
 val vaultBasePath: Path = Paths.get(vaultBase)
 
 fun readServiceUserCredentials() = ServiceUser(
