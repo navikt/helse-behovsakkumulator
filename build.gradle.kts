8b70fac2991383be95cfe20a1118520f31fd15d1
David Steinsland <david@steinsland.email>
2021-02-23 10:19:43 +0100

    bumper avhengigheter
    Co-authored-by: Christian Bülow Skovborg <christian.bulow.skovborg@nav.no>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,9 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val junitJupiterVersion = "5.7.0"
-val ktorVersion = "1.5.0"
 
 plugins {
-    kotlin("jvm") version "1.4.21"
+    kotlin("jvm") version "1.4.30"
 }
 
 group = "no.nav.helse"
@@ -10,34 +11,23 @@ group = "no.nav.helse"
 repositories {
     mavenCentral()
     maven { url = uri("https://jitpack.io") }
-    maven("https://kotlin.bintray.com/ktor")
-    maven("https://packages.confluent.io/maven/")
 }
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.6d6256d")
-    implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("io.ktor:ktor-client-auth-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-json-jvm:$ktorVersion")
-    implementation("io.ktor:ktor-client-jackson:$ktorVersion")
+    implementation("com.github.navikt:rapids-and-rivers:1.800727c")
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
     testImplementation("org.junit.jupiter:junit-jupiter-params:$junitJupiterVersion")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
-
-    testImplementation("no.nav:kafka-embedded-env:2.4.0")
-    testImplementation("org.awaitility:awaitility:4.0.3")
-
-    testImplementation("io.ktor:ktor-client-mock-jvm:$ktorVersion")
-    testImplementation("io.mockk:mockk:1.10.0")
 }
 
 tasks {
-    compileKotlin {
-        kotlinOptions.jvmTarget = "12"
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "14"
     }
-    compileTestKotlin {
-        kotlinOptions.jvmTarget = "12"
+
+    named<KotlinCompile>("compileTestKotlin") {
+        kotlinOptions.jvmTarget = "14"
     }
 
     named<Jar>("jar") {
