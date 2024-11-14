a1217da4f59e733bdc2d511230c74c958bbbaa03
david steinsland <david@davidsteinsland.net>
2024-11-14 09:36:48 +0100

    bumper tbd-libs

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 val junitJupiterVersion = "5.11.3"
 val rapidsAndRiversVersion = "2024111408081731568123.6619b48b7043"
-val tbdLibsVersion = "2024.11.12-20.46-e1fd6a25"
+val tbdLibsVersion = "2024.11.14-07.56-63a8564f"
 plugins {
     kotlin("jvm") version "2.0.21"
 }
