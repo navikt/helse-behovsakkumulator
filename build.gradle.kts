283e636db4967cecff4f1bc45c4e424d8e9af25e
david steinsland <david@davidsteinsland.net>
2025-03-27 17:59:18 +0100

    Bump Kotlin JVM -> 2.1.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ val junitJupiterVersion = "5.11.3"
 val rapidsAndRiversVersion = "2025012712551737978926.de930d8e0feb"
 val tbdLibsVersion = "2025.01.27-12.50-76316f3b"
 plugins {
-    kotlin("jvm") version "2.0.21"
+    kotlin("jvm") version "2.1.20"
 }
 
 group = "no.nav.helse"
