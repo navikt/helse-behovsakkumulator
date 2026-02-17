90b81692c70f22697b8b72187c64110601789478
Håvard Stigen Andersen <havard.stigen.andersen@gmail.com>
2026-02-17 10:04:37 +0100

    Bumper rapids and rivers versjonen

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 val junitJupiterVersion = "5.12.1"
-val rapidsAndRiversVersion = "2026011411051768385145.e8ebad1177b4"
+val rapidsAndRiversVersion = "2026021608421771227751.c2529c449f57"
 val tbdLibsVersion = "2026.01.22-09.16-1d3f6039"
 plugins {
     kotlin("jvm") version "2.2.21"
