54b1e1dbd35ae1343c5f1f22abc9be3549c2aee9
Svein Elgstøen <189128153+navelgen@users.noreply.github.com>
2026-04-16 13:46:06 +0200

    Bruk nytt, foretrukket oppsett for repoer

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,23 +7,6 @@ plugins {
 
 group = "no.nav.helse"
 
-// Sett opp repositories basert på om vi kjører i CI eller ikke
-// Jf. https://github.com/navikt/utvikling/blob/main/docs/teknisk/Konsumere%20biblioteker%20fra%20Github%20Package%20Registry.md
-repositories {
-    mavenCentral()
-    if (providers.environmentVariable("GITHUB_ACTIONS").orNull == "true") {
-        maven {
-            url = uri("https://maven.pkg.github.com/navikt/maven-release")
-            credentials {
-                username = "token"
-                password = providers.environmentVariable("GITHUB_TOKEN").orNull!!
-            }
-        }
-    } else {
-        maven("https://repo.adeo.no/repository/github-package-registry-navikt/")
-    }
-}
-
 dependencies {
     implementation("com.github.navikt:rapids-and-rivers:$rapidsAndRiversVersion")
     implementation("io.valkey:valkey-java:5.5.0")
